432716565827364fb67f12cec73956237d11315c
Damian Bravo <Dbravo1005@gmail.com>
2023-04-02 20:37:15 -0400

    index.html

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+newHeader.id = 'victory';
+//const victory = document.getElementById('victory');
+newHeader.textContent = '"Damian" is the champion'
+
+    
